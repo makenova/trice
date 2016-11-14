f63b0a6ca35581f31ca464f3b88291d9cf2ba309
makenova <themarchmyth@gmail.com>
2016-11-14 11:31:17 -0600

    Add format method
    update o-clock dependency
    add tests
    add ‘use strict’ for node v4 tests

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-var oclock = require('o-clock')
-var ordinal = require('ordinal-numbers')
+'use strict';
+
+let oclock = require('o-clock')
+let ordinal = require('ordinal-numbers')
+
+function parseDate(object, date) {
+  let dateString = date.toDateString()
+
+  object.day = dateString.split(' ')[0]
+  object.month = dateString.split(' ')[1]
+  object.monthDate = ordinal(date.getDate())
+  object.year = date.getFullYear()
+}
+
+function parseTime(object, date) {
+  let timeString = date.toTimeString().split(' ')[0]
+  let oclockString = oclock(date)
+
+  object.hour = oclockString.split(':')[0]
+  object.minutes = timeString.split(':')[1]
+  object.seconds = timeString.split(':')[2]
+  object.meridiem = oclockString.split(' ')[1]
+}
 
 class Occasion {
   constructor(date) {
     this.date = date || new Date()
+    this.formatKey = {
+      'Y': 'year',
+      'M': 'month',
+      'D': 'monthDate',
+      'd': 'day',
+      'h': 'hour',
+      'm': 'minutes',
+      's': 'seconds',
+      'p': 'meridiem',
+    }
+
+    parseDate(this, this.date)
+    parseTime(this, this.date)
   }
 
   valueOf() {
@@ -15,12 +49,7 @@ class Occasion {
   }
 
   getDate() {
-    var dateString = this.date.toDateString()
-    var day = dateString.split(' ')[0]
-    var month = dateString.split(' ')[1]
-    var date = ordinal(this.date.getDate())
-
-    return `${day}, ${month} ${date} ${this.date.getFullYear()}`
+    return `${this.day}, ${this.month} ${this.monthDate} ${this.year}`
   }
 
   getTime() {
@@ -30,17 +59,10 @@ class Occasion {
   format(formatString){
     if(!formatString) return  this.date.toString()
 
-    // TODO parse format string and return formatted
-    // formatString === 'd M D Y' || 'h : m : s mm'
-    // d day of the week
-    // M month
-    // D date of the month
-    // Y year
-    // h hour
-    // m minutes
-    // s seconds
-    // mm meridiem
-    return  this.date.toString()
+    return formatString
+      .split('')
+      .map(value => this[this.formatKey[value]] || value)
+      .join('')
   }
 }
 
--- test.js
+++ test.js
@@ -1,17 +1,17 @@
 import test from 'ava'
 import trice from './index'
 
-test("new trice constructor without arguments", t => {
+test('new trice constructor without arguments', t => {
   let date = new Date()
   t.is(new trice().toString(), date.toString())
 })
 
-test("new trice constructor with arguments", t => {
+test('new trice constructor with arguments', t => {
   let date = new Date()
   t.is(new trice(date).toString(), date.toString())
 })
 
-test("trice format without arguments", t => {
+test('trice format without arguments', t => {
   let date = new Date()
   t.is(new trice(date).format(), date.toString())
 })
@@ -26,7 +26,25 @@ test('trice getTime', t => {
   t.is(new trice(date).getTime(), '9:45 PM')
 })
 
-test.skip("trice format with arguments", t => {
+test("trice format with arguments 'day month date year'", t => {
   let date = new Date()
-  t.is(new trice(date).format('d M D Y'), date.toDateString())
+  let occasion = new trice(date)
+  t.is(occasion.format('d, M D Y'), occasion.getDate())
+})
+
+test("trice format match getTime()", t => {
+  let date = new Date(2016, 10, 13, 21, 45, 30)
+  let occasion = new trice(date)
+  t.is(occasion.format('h:m p'), occasion.getTime())
+})
+
+test("trice format with arguments 'hour minutes seconds meridiem'", t => {
+  let date = new Date(2016, 10, 13, 21, 45, 30)
+  t.is(new trice(date).format('h:m:s p'), '9:45:30 PM')
+})
+
+test("all fromat options", t => {
+  let date = new Date(2016, 10, 13, 21, 45, 30)
+  let occasion = new trice(date)
+  t.is(occasion.format('d, M D Y h:m:s p'), 'Sun, Nov 13th 2016 9:45:30 PM')
 })
